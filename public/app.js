2efa92407d22838802d3321197b5a86502407b1c
John Shepler <john.sheplerjr@gmail.com>
2017-05-02 21:24:17 -0600

    something

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
+$(document).ready(function() {
+    $(".rollTo").click(function() {
+        var myLink = $(this).find('a').attr('href')
+        console.log(myLink)
+        $('html, body').animate({
+            scrollTop: $(myLink).offset().top
+        }, 1000);
+        console.log(this);
+    });
 
-$(document).ready(function(){$(".rollTo").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#skills").offset().top
-    }, 1000);
-    console.log(this);
-});
+     $(".owl-slider").owlCarousel({
+         autoPlay: 7000, //Set AutoPlay to 3 seconds
+         items: 3,
+         itemsDesktop: [1199, 3],
+         itemsDesktopSmall: [979, 3]
+
+     });
+     $("a[rel^='prettyPhoto']").prettyPhoto();
 });
